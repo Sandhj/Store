c4658ca0bc3e1cfcc2df97065660ce013dfdd02e
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-16 11:39:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ python3 -m venv web
 wget -q https://raw.githubusercontent.com/Sandhj/Store/main/app.py
 
 cd templates
-wget -q https://raw.githubusercontent.com/Sandhj/Store/main/index.html /root/project/templates/
-wget -q https://raw.githubusercontent.com/Sandhj/Store/main/result.html /root/project/templates/
+wget -q https://raw.githubusercontent.com/Sandhj/Store/main/index.html
+wget -q https://raw.githubusercontent.com/Sandhj/Store/main/result.html
 cd
 
 cd /root/project/static
-wget -q https://raw.githubusercontent.com/Sandhj/Store/main/styles.css/root/project/static/
+wget -q https://raw.githubusercontent.com/Sandhj/Store/main/styles.css
 
 cd
 rm setup.sh
