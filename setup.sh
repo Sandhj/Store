0b6da47d2bdd2edc3b8e494891f1d5b3962e118d
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-16 17:14:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,15 @@ cd /root/project/
 mkdir -p templates
 python3 -m venv web
 
+pip install flask
+pip install requests
+
 wget -q https://raw.githubusercontent.com/Sandhj/Store/main/app.py
 
 cd templates
 wget -q https://raw.githubusercontent.com/Sandhj/Store/main/index.html
 wget -q https://raw.githubusercontent.com/Sandhj/Store/main/result.html
+wget -q https://raw.githubusercontent.com/Sandhj/Store/main/create.html
+wget -q https://raw.githubusercontent.com/Sandhj/Store/main/renew.html
 cd
 rm setup.sh
